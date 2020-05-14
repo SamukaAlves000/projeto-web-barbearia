e54081ff29ace0065ad75e1596751eb8f44b0eef
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-05-14 09:53:38 -0300

    Refatorando projeto
    Refatorando projeto

--- barbearia/src/main/java/com/samuel/resource/util/ApplicationResource.java
+++
@@ -1,9 +0,0 @@
-package com.samuel.resource.util;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ApplicationResource extends Application {
-
-}
