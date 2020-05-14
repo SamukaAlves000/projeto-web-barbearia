f277e6123cc5c9b8f2e709ef9895ab1abe7b2ff2
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-05-14 09:59:42 -0300

    Criando projeto Spring Boot
    Criando projeto Spring Boot

---
+++ barbearia/src/main/java/br/com/samuel/barbearia/BarbeariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.barbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaApplication.class, args);
+	}
+
+}
---
+++ barbearia/src/test/java/br/com/samuel/barbearia/BarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuel.barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
